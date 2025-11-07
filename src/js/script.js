91d10748f1667c19f8790071d1693786b2e0b352
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2025-11-07 14:54:45 +0100

    ouverture menu + MENU.CSS

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+
+
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+
+
+toggle.addEventListener("click", () => {
+  nav.ariaHidden = false;
+  toggle.ariaExpanded = true;
+});
+
