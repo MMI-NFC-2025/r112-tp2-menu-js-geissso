11526f1c85e8406ac531113cfbfd7513918f7c31
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2025-11-07 15:04:26 +0100

    ouverture et fermeture du menu

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,15 @@ const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 
 
-toggle.addEventListener("click", () => {
-  nav.ariaHidden = false;
-  toggle.ariaExpanded = true;
-});
 
+
+
+toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+  nav.ariaHidden = isOpen;
+  toggle.ariaExpanded = isClosed;
+  
+  console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+
+});
